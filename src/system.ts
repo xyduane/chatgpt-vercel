65ec1b3512d871c458f459e125971f8eb20c6e65
xyun <xyduane@qq.com>
2023-04-01 00:23:44 +0800

    xx

--- src/system.ts
+++ src/system.ts
@@ -10,9 +10,9 @@ export const defaultSetting = {
   model: "gpt-3.5-turbo" as Model
 }
 
-export const defaultMessage = `Powered by OpenAI dxy
+export const defaultMessage = `Powered by OpenAI 
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。
-- 请勿对外传播
+- 私有部署，请勿对外传播，谢谢
 `
 
 export type Setting = typeof defaultSetting
